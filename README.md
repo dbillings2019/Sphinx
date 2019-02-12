7b4c7565ee702bbc9f53ad22f09e4081ea8a8417
userpc <userpc@mail.com>
2019-02-12 10:41:11 -0800

    still needs counters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Sphinx
+The Sphinx 1.0
+
+Player clicks on each relic represented by an icon in the north, west, and south directions. A riddle will be presented and the player must choose one of the answers to earn the relic. If the answer is incorrect, however, the player loses one life. Each relic can only be won once but may be attempted to be won as long it has not been correctly answered. Once the three questions are correctly answered, the player must click on the sphinx to end the game.
---
+++ js/app.js
@@ -0,0 +1,145 @@
+var tools = document.getElementById("tools");
+var ankh = document.getElementById("ankh");
+var caduces = document.getElementById("caduces");
+var eyeRa = document.getElementById("eyeRa");
+var sphinx = document.getElementById("sphinx");
+
+var lives = 3;
+var relics = 0;
+var i = 0;
+
+var container = document.getElementById("container");
+var container2 = document.getElementById("container2");
+var trivia = document.getElementById("trivia");
+var next = document.getElementById("next");
+var currQues = document.getElementById("currQues");
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+
+var questions = [
+    {question: "When the wind blows north, where does a peanut falling from a tree land?",
+    answers: {
+        a: "North",
+        b: "South",
+        c: "Neither"
+    },
+    correctAnswer:"c",
+    marked: false
+    },
+    {question: "Do they have a 4th of July in England?",
+    answers: {
+        a: "Yes",
+        b: "No",
+        c: "Maybe"
+    },
+    correctAnswer:"a",
+    marked: false
+},
+{question: "Some months have 30 days, some months have 31. So how many months have 28 days?",
+    answers: {
+        a: "3",
+        b: "1",
+        c: "12"
+    },
+    correctAnswer:"c",
+    marked: false
+},
+    {question: "Can a man living in the US be buried in Canada?",
+    answers: {
+        a: "Yes",
+        b: "Maybe",
+        c: "No"
+    },
+    correctAnswer:"c",
+    marked: false
+    },
+    {question: "A blue house is made with blue bricks. A red house is made with red bricks. A yellow house is made with yellow bricks. What is a greenhouse made with?",
+    answers: {
+        a: "More information is needed",
+        b: "Glass",
+        c: "Bricks"
+    },
+    correctAnswer:"b",
+    marked: false
+    }
+];
+
+container.style.visibility = "hidden";
+container2.style.visibility = "hidden";
+next.style.visibility = "hidden";
+trivia.style.visibility = "hidden";
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("start").addEventListener('click', function() {
+        container.style.visibility = "visible";
+        container2.style.visibility = "visible";
+        
+        document.getElementById("start").style.visibility = "hidden";
+        
+    })
+
+    
+    next.addEventListener('click', function(){
+        console.log("Question Submitted");
+        next.style.visibility = "hidden";
+        trivia.style.zIndex = "-100";
+        i++;
+    })
+    
+    //radio buttons
+    
+    ankh.addEventListener('click', function(){
+        console.log("Clicking ankh...");
+        if (questions[i].marked === false)
+        next.style.visibility = "visible";
+        trivia.style.visibility = "visible";
+        trivia.style.zIndex = "1";
+        currQues.textContent = questions[i].question;
+        a.textContent = questions[i].answers.a;
+        b.textContent = questions[i].answers.b;
+        clearTimeout.textContent = questions[i].answers.c;
+    })
+
+    caduces.addEventListener('click', function(){
+        console.log("Clicking caduces...");
+        if (questions[i].marked === false)
+        next.style.visibility = "visible";
+        trivia.style.visibility = "visible";
+        trivia.style.zIndex = "1";
+        currQues.textContent = questions[i].question;
+        a.textContent = questions[i].answers.a;
+        b.textContent = questions[i].answers.b;
+        clearTimeout.textContent = questions[i].answers.c;
+    })
+
+    eyeRa.addEventListener('click', function(){
+        console.log("Clicking eyeRa...");
+        if (questions[i].marked === false)
+        next.style.visibility = "visible";
+        trivia.style.visibility = "visible";
+        trivia.style.zIndex = "1";
+        currQues.textContent = questions[i].question;
+        a.textContent = questions[i].answers.a;
+        b.textContent = questions[i].answers.b;
+        clearTimeout.textContent = questions[i].answers.c
+    })
+
+    sphinx.addEventListener('click', function(){
+        console.log("To pass or to go");
+        if (relics === 3){
+            alert("You win the game!");
+            location.reload();
+        } else {
+            alert("You must gain more relics!")
+        }
+        //animate towards sphinx
+        
+
+
+    })
+
+    
+})
